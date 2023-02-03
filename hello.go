70b16b21cd5bab1655615009a8b9fd7e8fa68075
annar40 <80236565+annar40@users.noreply.github.com>
2023-02-03 10:03:30 -0500

    new loginand sign up structure

--- hello.go
+++ hello.go
@@ -6,36 +6,48 @@ import (
 	"net/http"
 )
 
-// User struct to hold login credentials
-type User struct {
-	UserName string
-	Password string
+var userDB = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
 }
 
-// this is a test
-// login page handler function
 func login(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
+	switch r.Method {
+	case http.MethodGet:
 		t, _ := template.ParseFiles("login.html")
 		t.Execute(w, nil)
-	} else {
+	case http.MethodPost:
 		r.ParseForm()
-		// extract the username and password from the form
-		u := User{
-			UserName: r.FormValue("username"),
-			Password: r.FormValue("password"),
-		}
-
-		// check if the login credentials are correct
-		if u.UserName == "admin" && u.Password == "password" {
-			fmt.Fprint(w, "Welcome, "+u.UserName+"! You are logged in.")
+		username := r.Form.Get("username")
+		password := r.Form.Get("password")
+		if p, ok := userDB[username]; ok && p == password {
+			fmt.Fprintln(w, "Login Successful")
 		} else {
-			fmt.Fprint(w, "Invalid login credentials. Please try again.")
+			fmt.Fprintln(w, "Login Failed")
+		}
+	}
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		t, _ := template.ParseFiles("signup.html")
+		t.Execute(w, nil)
+	case http.MethodPost:
+		r.ParseForm()
+		username := r.Form.Get("username")
+		password := r.Form.Get("password")
+		if _, ok := userDB[username]; !ok {
+			userDB[username] = password
+			fmt.Fprintln(w, "Signup Successful")
+		} else {
+			fmt.Fprintln(w, "Username already taken")
 		}
 	}
 }
 
 func main() {
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/signup", signup)
 	http.ListenAndServe(":8000", nil)
 }
