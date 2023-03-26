93844890a8d786ab8c2dd76c409ee06349ff5a33
annar40 <80236565+annar40@users.noreply.github.com>
2023-03-26 13:55:09 -0400

    unit testing for journal handler passing

--- hello_test.go
+++ hello_test.go
@@ -110,3 +110,55 @@ func TestSignupHandler(t *testing.T) {
 		t.Errorf("user not added to Firestore correctly: got %v, want %v", userData, user)
 	}
 }
+func TestJournalHandler(t *testing.T) {
+
+	// Create a new login request with a JSON body
+	user := User{Name: "catherine", Email: "123@gmail.com", Password: "12345"}
+	jsonBody, _ := json.Marshal(user)
+	req, _ := http.NewRequest("POST", "/login", bytes.NewReader(jsonBody))
+
+	// Create a new journal entry request with a JSON body
+	entry := Entry{JournalEntry: "journal handler test working"}
+	jsonBody2, _ := json.Marshal(entry)
+	req2, _ := http.NewRequest("POST", "/journalEntry", bytes.NewReader(jsonBody2))
+
+	// Create a new recorder for capturing the response
+	rr := httptest.NewRecorder()
+
+	// Initialize Firebase app
+	ctx := context.Background()
+	config := &firebase.Config{
+		ProjectID: "thoughtdump-4b31d",
+	}
+	opt := option.WithCredentialsFile("./serviceAccountKey.json")
+	app, err := firebase.NewApp(ctx, config, opt)
+	if err != nil {
+		t.Fatalf("error initializing app: %v", err)
+	}
+
+	// Create Firestore client
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		t.Fatalf("error creating Firestore client: %v", err)
+	}
+	defer client.Close()
+
+	// Call the login handler
+	loginhandler := http.HandlerFunc(loginHandler(client))
+	c := cors.Default().Handler(loginhandler)
+	c.ServeHTTP(rr, req)
+
+	// Call the journal handler
+	entryhandler := http.HandlerFunc(journalHandler(client))
+	c2 := cors.Default().Handler(entryhandler)
+	c2.ServeHTTP(rr, req2)
+
+	// Check that the response is as expected
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v, want %v", status, http.StatusOK)
+	}
+	expected := "Login SuccessfulEntry data written to Firestore"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v, want %v", rr.Body.String(), expected)
+	}
+}
