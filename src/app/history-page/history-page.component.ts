793c5c56f47475a5065a95611ff210b2b9e09391
dawleyj <jake.dawley21@gmail.com>
2023-04-18 14:43:43 -0400

    Added code for highlighting dates (not working)

--- src/app/history-page/history-page.component.ts
+++ src/app/history-page/history-page.component.ts
@@ -3,6 +3,8 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { formatDate } from '@angular/common';
+import { MatCalendarCellCssClasses } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-history-page',
@@ -14,6 +16,9 @@ export class HistoryPageComponent {
   today: Date = new Date();
   journalEntry: any; // variable to store the journal entry response
   submittedDates: any;
+  date: any;
+  isDateLoaded: boolean = false;
+  isHighLoaded: boolean = false;
 
   
   constructor(
@@ -26,11 +31,37 @@ export class HistoryPageComponent {
     this.httpClient.get<any>('http://localhost:8000/retrieveDates').subscribe(data =>{
       console.log('Get dates: ', data.dates);
       this.submittedDates = data.dates;
+      console.log('submitted dates', this.submittedDates);
+      this.isDateLoaded = true;
+      /*The boolean is set to true once the dates are returned from the backend so that the calendar can load first.
+      The ngIf in the html file keeps the calendar from loading until it's true.
+      */
     }, error  =>{
       console.log('Error getting dates', error);
-    });
-
+    }
+    );
   }
+
+
+  
+  highlightDate(date: Date): string {
+    if (date == null) {
+      return '';
+    }
+    const formattedDate = date.toLocaleDateString('en-CA', { year: 'numeric', month: '2-digit', day: '2-digit' });
+    console.log('formatted',formattedDate);
+    /*The problem lies here, because for some reason isDateLoaded and the submittedDates array get turned to undefined inside
+    this function even though they were set in the ngOnInit function. If we can get them to hold their value the calendar should
+    properly be highlighted, i.e. everything else should be working already.
+    */
+    console.log(this.submittedDates);
+    console.log('dateLoaded',this.isDateLoaded);
+    const isHighlighted =  this.submittedDates.includes(formattedDate);
+    
+    return isHighlighted ? 'highlighted-date' : '';
+  }
+  
+  
 
   submitDate() {
     if (this.selected) {
